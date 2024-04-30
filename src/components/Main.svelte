a76804d9de48a70caaf4c7c82066b68dcc43efa3
LucaParenti <luca.parenti.work@gmail.com>
2024-04-30 14:20:15 +0200

    Fixed arrow in button

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -15,7 +15,7 @@
             <!-- svelte-ignore a11y-invalid-attribute -->
             <a href="#" target="_blank" class="blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer">
                 <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"/>
-                <h4 class="relative z-9">Get in touch &rarr;</h4>
+                <h4 class="relative z-9">Get in touch <i class="fa-solid fa-arrow-right ml-1 text-base"></i></h4>
             </a>
         </div>
         <div class="relative shadow-2xl grid place-items-center">
