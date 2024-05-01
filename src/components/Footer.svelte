a3a8671f6ef1e122c61cb33a44b3860d8423a81f
LucaParenti <luca.parenti.work@gmail.com>
2024-05-01 11:28:35 +0200

    Corrected connect with me section in footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,6 +1,6 @@
 <footer id="contacts" class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center">
-    <p class="px-4 py-2 bg-white text-slate-950 font-medium">
-        Connect with me &darr;
+    <p class="px-4 py-2 bg-white text-slate-950 font-medium rounded-lg">
+        Connect with me <i class="fa-solid fa-arrow-down ml-1 text-base"></i>
     </p>
     <div class="flex flex-col gap-4 items-center justify-center">
         <p><b class="pr-2">Email:</b> <a href="mailto:luca.parenti.work@gmail.com" class="text-violet-400">
