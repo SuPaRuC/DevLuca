03fb18901367525c3c087c29059eee6b34d8d827
LucaParenti <luca.parenti.work@gmail.com>
2024-05-01 10:03:33 +0200

    Finished projects section

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -14,7 +14,7 @@
     <h1 class="font-medium">
         <b class="font-bold poppins">Luca</b> <span class="">Parenti</span>
     </h1>
-    <div class="sm:flex items-center gap-4 hidden">
+    <div class="sm:flex ml-auto pr-4 items-center gap-4 hidden">
         {#each tabs as tab, index}
             <a href={tab.link} class="duration-200 hover:text-violet-400" target={index === 2 ? "_blank" : ""}>
                 <p>{tab.name}</p>
--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,5 +1,10 @@
 <script>
+    import Step from "../components/Step.svelte";
 
+    let steps = [
+        {name: 'Performance evaluation of a microcloud service of task oﬄoading in vehicular edge computing', icon: 'fa-solid fa-wifi', href: 'assets/task_offloading.zip'},
+        {name: 'MeteoCity', icon: 'fa-solid fa-cloud', href: 'https://github.com/SuPaRuC/MeteoCity'}
+    ];
 </script>
 
 <main class="flex flex-col flex-1 p-4">
@@ -23,6 +28,7 @@
                 <i class="fa-brands fa-git-alt"></i>
             </p>
             <!-- svelte-ignore a11y-invalid-attribute -->
+            <!-- TODO insert link to contacts -->
             <a href="#" target="_blank" class="blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer">
                 <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"/>
                 <h4 class="relative z-9">Get in touch <i class="fa-solid fa-arrow-right ml-1 text-base"></i></h4>
@@ -31,6 +37,28 @@
         <div class="relative shadow-2xl grid place-items-center">
             <!-- svelte-ignore a11y-img-redundant-alt -->
             <img src={"images/profile.png"} alt="Profile image" class="object-cover z-[2] max-h-[70vh]">
+        </div>
+    </section>
+
+    <!-- TODO Finish description of projects -->
+    <section id="projects" class="py-20 lg:py32 flex flex-col gap-24">
+        <div class="flex flex-col gap-2 text-center">
+            <h6 class="text-lg sm:text-xl md:text-2xl">
+                A few of my projects
+            </h6>
+            <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+                Curious to <span class="poppins text-violet-400">see</span> my work?
+            </h3>
+        </div>
+        <div class="grid grid-cols-1 lg:grid-cols-2 gap-12 lg:gap-10">
+            <Step step={steps[0]}>
+                <p>
+                    This project focus on <strong class="text-violet-400">C++</strong>
+                </p>
+            </Step>
+
+            <Step step={steps[1]}>
+            </Step>
         </div>
     </section>
 </main>
