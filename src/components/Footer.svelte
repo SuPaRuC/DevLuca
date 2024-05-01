c76786347049ec76ce4f3e85d11eb79864464679
LucaParenti <luca.parenti.work@gmail.com>
2024-05-01 15:22:27 +0200

    Disabled black background in footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,4 +1,4 @@
-<footer id="contacts" class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center">
+<footer id="contacts" class="py-20 sm:py-32 border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center">
     <p class="px-4 py-2 bg-white text-slate-950 font-medium rounded-lg">
         Connect with me <i class="fa-solid fa-arrow-down ml-1 text-base"></i>
     </p>
