9618404cdd44c52609699fc1261f9bae5a49bb5b
LucaParenti <luca.parenti.work@gmail.com>
2024-05-01 10:46:18 +0200

    Footer finished

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -35,7 +35,7 @@
             </p>
             <!-- svelte-ignore a11y-invalid-attribute -->
             <!-- TODO insert link to contacts -->
-            <a href="#" target="_blank" class="blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer">
+            <a href="#" target="_blank" class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer">
                 <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"/>
                 <h4 class="relative z-9">Get in touch <i class="fa-solid fa-arrow-right ml-1 text-base"></i></h4>
             </a>
