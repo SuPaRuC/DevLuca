90d8ef58b13da9d193e64e02e585b26b4d20615e
LucaParenti <luca.parenti.work@gmail.com>
2024-05-02 10:06:37 +0200

    Added file and modified configurations

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,7 @@ export default {
 			strict: true,
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/DevLuca' : '',
+			base: process.argv.includes('dev') ? '' : '/DevLuca',
 		}
 	}
 };
