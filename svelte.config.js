8dc3b602f922c4c5532a9a4186d586ab07ab2b51
LucaParenti <luca.parenti.work@gmail.com>
2024-05-02 09:18:44 +0200

    Added static build for website

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-  kit: {
+export default {
+	kit: {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: undefined,
+			fallback: 'app.html',
 			precompress: false,
 			strict: true
 		})
-	},
-  preprocess: vitePreprocess()
+	}
 };
 
-export default config;
+export const prerender = true;
